339aeaf74228958fe19712b395e218e00c00a62d
Yami <yamikadesu@outlook.es>
2026-08-03 00:58:09 +0200

    Updated versions

--- scripts/lust.lua
+++ scripts/lust.lua
@@ -1,6 +1,6 @@
 local mod = RegisterMod("Lust (YamikaDesu)", 1) 
 
-local version = "1.9.4"
+local version = "1.9.6"
 local debugString = mod.Name .. " V" .. version .. " loaded successfully"
 print(debugString)
 
